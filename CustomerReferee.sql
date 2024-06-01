9fc037ec0c216703114e99d56ea713adf1f74fc9
Arpita Shukla <arpitashukla802@gmail.com>
2024-06-01 16:57:48 +0000

    SQL_50

---
+++ CustomerReferee.sql
@@ -0,0 +1,3 @@
+#584
+
+SELECT name FROM Customer Where referee_id != 2 OR referee_id IS null;
--- RecylableLowFatProducts.sql
+++ RecylableLowFatProducts.sql
@@ -1,5 +1,4 @@
-//For selecting the product ids that are both low fat and recyclable.\n
-  LEETCODE 1757
+#LEETCODE 1757
 
 SELECT product_id FROM Products
 WHERE low_fats = 'Y' AND recyclable = 'Y';
